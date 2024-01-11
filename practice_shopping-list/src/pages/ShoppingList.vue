72faa1823c2656f1698fb48874cb51ff450ab290
yuning-dev <yuning.eng@gmail.com>
2024-01-11 12:59:59 +0000

    add mark all as complete and mark all as incomplete buttons, including functionality. Add check so the modal window doesn't trigger if all items are already complete or incomplete (respectively). Also add check for the delete all button so the modal window doesn't trigger when there are no items.

--- practice_shopping-list/src/pages/ShoppingList.vue
+++ practice_shopping-list/src/pages/ShoppingList.vue
@@ -1,16 +1,35 @@
-
-import ShoppingItemVue from '@/components/ShoppingItem.vue';
 <template>
-    <template v-if="hasModal">
-        <ModalWindow @closeModal="hasModal=false">
+    <template v-if="hasDeleteAllModal">
+        <ModalWindow @closeModal="hasDeleteAllModal=false">
             <template v-slot>
                 Are you sure you want to delete all items?
                 <div :class="$style.modalBtnsWrapper">
                     <button :class="$style.yesBtn" @click="deleteAllItems">Yes</button>
-                    <button :class="$style.cancelBtn" @click="hasModal=false">Cancel</button>
+                    <button :class="$style.cancelBtn" @click="hasDeleteAllModal=false">Cancel</button>
                 </div>
             </template>
-
+        </ModalWindow>
+    </template>
+    <template v-if="hasMarkAsCompletedModal">
+        <ModalWindow @closeModal="hasMarkAsCompletedModal=false">
+            <template v-slot>
+                Are you sure you want to mark all items as completed?
+                <div :class="$style.modalBtnsWrapper">
+                    <button :class="$style.yesBtn" @click="markAllAsCompleted">Yes</button>
+                    <button :class="$style.cancelBtn" @click="hasMarkAsCompletedModal=false">Cancel</button>
+                </div>
+            </template>
+        </ModalWindow>
+    </template>
+    <template v-if="hasMarkAsIncompleteModal">
+        <ModalWindow @closeModal="hasMarkAsIncompleteModal=false">
+            <template v-slot>
+                Are you sure you want to mark all items as incomplete?
+                <div :class="$style.modalBtnsWrapper">
+                    <button :class="$style.yesBtn" @click="markAllAsIncomplete">Yes</button>
+                    <button :class="$style.cancelBtn" @click="hasMarkAsIncompleteModal=false">Cancel</button>
+                </div>
+            </template>
         </ModalWindow>
     </template>
     <div :class="$style.wrapper">
@@ -26,7 +45,11 @@ import ShoppingItemVue from '@/components/ShoppingItem.vue';
             </template>
         </div>
         <div :class="$style.bottomBtnsWrapper">
-            <button :class="$style.deleteAllBtn" @click="hasModal=true">Delete all items</button>
+            <button :class="$style.deleteAllBtn" @click="deleteAllBtnClicked">Delete all items</button>
+            <div :class="$style.bottomLeftBtnsWrapper">
+                <button :class="$style.markAllBtns" @click="markAllAsCompletedBtnClicked">Mark all as completed</button>
+                <button :class="$style.markAllBtns" @click="markAllAsIncompleteBtnClicked">Mark all as incomplete</button>
+            </div>            
         </div>
     </div>
 </template>
@@ -47,7 +70,9 @@ export default {
             newItemContent: '',
             newItemId: 1,
             shoppingList: [],
-            hasModal: false,
+            hasDeleteAllModal: false,
+            hasMarkAsCompletedModal: false,
+            hasMarkAsIncompleteModal: false,
         }
     },
     methods: {
@@ -74,11 +99,55 @@ export default {
             const listWithoutItemToBeDeleted = this.shoppingList.filter((item) => item.id !== id)
             this.shoppingList = listWithoutItemToBeDeleted
         },
+        deleteAllBtnClicked() {
+            if (this.shoppingList.length === 0) {
+                return
+            } else {
+                this.hasDeleteAllModal = true
+            }
+        },
         deleteAllItems() {
             this.shoppingList = []
-            this.hasModal = false
+            this.hasDeleteAllModal = false
         },
-
+        markAllAsCompletedBtnClicked() {
+            let counter = 0
+            for (let i = 0; i < this.shoppingList.length; i++) {
+                if (this.shoppingList[i].completion === true) {
+                    counter++
+                }
+            }
+            if (counter === this.shoppingList.length) {
+                this.hasMarkAsCompletedModal = false
+            } else {
+                this.hasMarkAsCompletedModal = true
+            }
+        },
+        markAllAsCompleted() {
+            for (let i = 0; i < this.shoppingList.length; i++) {
+                this.shoppingList[i].completion = true
+            }
+            this.hasMarkAsCompletedModal = false
+        },
+        markAllAsIncompleteBtnClicked() {
+            let counter = 0
+            for (let i = 0; i < this.shoppingList.length; i++) {
+                if (this.shoppingList[i].completion === true) {
+                    counter++
+                }
+            }
+            if (counter === 0) {
+                this.hasMarkAsCompletedModal = false
+            } else {
+                this.hasMarkAsIncompleteModal = true
+            }
+        },
+        markAllAsIncomplete() {
+            for (let i = 0; i < this.shoppingList.length; i++) {
+                this.shoppingList[i].completion = false
+            }
+            this.hasMarkAsIncompleteModal = false
+        }
     },
 }
 
